78f9657e742be672d8169842e5c87260a2671d85
Renuja-7671 <renujajanith7671@gmail.com>
2025-07-22 22:13:10 +0530

    register included in the middle

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { register, login, resetPassword, forgotPassword } = require('../controllers/authController');
 
 
-router.post('/register', register);
 router.post('/login', login);
+router.post('/register', register);
 router.post("/forgot-password", forgotPassword);
 router.post('/reset-password', resetPassword);
 
