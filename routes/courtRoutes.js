b92aaf92f3f9a93f123bb5583de11e0fbe8c6bf8
Renuja-7671 <renujajanith7671@gmail.com>
2025-07-23 03:40:18 +0530

    Arena Image test2

--- routes/courtRoutes.js
+++ routes/courtRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const courtController = require("../controllers/courtController");
 const { authenticateUser, authorizeRole } = require("../middleware/authMiddleware");
-const { uploadCourts } = require("../middleware/cloudinaryUpload");
+const { upload } = require("../middleware/cloudinaryUpload");
 
 //Routes to create new court
-router.post("/upload-images", authenticateUser, authorizeRole(["Owner"]), uploadCourts.array("images"), courtController.uploadCourtImages);
+router.post("/upload-images", authenticateUser, authorizeRole(["Owner"]), upload.array("images"), courtController.uploadCourtImages);
 router.post("/create", authenticateUser, authorizeRole(["Owner"]), courtController.createCourt);
 
 router.get("/arenas/:arenaId/courts", authenticateUser, authorizeRole(["Owner"]), courtController.getCourtsByArena); // Get courts by arena
